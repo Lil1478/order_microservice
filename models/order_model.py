b5bb3920a312851043c32f63534841eeb654a03b
lnekh <nekhviadovichl@gmail.com>
2022-04-21 21:32:01 +0200

    fixes

--- models/order_model.py
+++ models/order_model.py
@@ -7,15 +7,21 @@ class Order(Base):
     __tablename__ = 'Orders'
     order_id = Column(Integer, primary_key=True)
     name = Column(String(120), nullable=False)
+    user_id = Column(Integer, nullable=False)
+    product_id = Column(Integer, nullable=False)
     price = Column(Integer(), nullable=False)
 
-    def __init__(self, name, price):
+    def __init__(self, name, price, user_id, product_id):
         self.name = name
+        self.user_id = user_id
+        self.product_id = product_id
         self.price = price
 
     def to_json(self):
         return {
             'id': self.order_id,
             'name': self.name,
+            'user_id': self.user_id,
+            'product_id': self.order_id,
             'price': self.price
         }
--- order_module/order_repository.py
+++ order_module/order_repository.py
@@ -9,26 +9,27 @@ class OrderRepository:
         print("OrderRepository")
 
     def add_order(self, order: Order):
-        # return self.account_dao.add_order(order)
-        return "order added"
+        return self.account_dao.add_order(order)
+        # return "order added"
 
     def get_orders(self):
-        # return self.account_dao.get_all()
-        return "order getted"
+        return self.account_dao.get_all()
+        # return "order getted"
 
     def get_order_by_id(self, order_id):
-        order = {"user_id": 1, "product_id": 1, "name": "order_1", "price": 1}
+        # order = {"user_id": 1, "product_id": 1, "name": "order_1", "price": 1}
 
-        user = requests.get('http://localhost:8000/users/%s' % order['user_id']).json()
-        product = requests.get('http://localhost:4000/products/%s' % order['product_id']).json()
+        order = self.account_dao.get_order(order_id)
+        user = requests.get('http://localhost:8000/users/%s' % order.user_id).json()
+        product = requests.get('http://localhost:4000/products/%s' % order.product_id).json()
 
         print("user_id: " + str(user) + " producr_id" + str(product))
 
         return {"user_first_name": user['first_name'], "user_last_name": user['last_name'],
                 "product_name": product['name'], "product_price": product['price']}
         # return self.account_dao.get_order(order_id)
-        return "order getted by id"
+        # return "order getted by id"
 
     def delete_order(self, order_id):
-        # return self.account_dao.delete_order(order_id)
-        return "order deleted"
+        return self.account_dao.delete_order(order_id)
+        # return "order deleted"
